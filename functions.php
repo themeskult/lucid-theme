a263ba6c70f3b5f1530e430cd769aa43804183c1
Ricardo Rauch <rickyrauch@gmail.com>
2013-09-26 03:05:19 -0300

    updating system

--- functions.php
+++ functions.php
@@ -215,22 +215,4 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/jetpack.php';
 
 
-include_once('inc/WordPress-GitHub-Plugin-Updater/updater.php');
-
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/themeskult/lucid-theme', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/themeskult/lucid-theme/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/themeskult/lucid-theme', // the github url of your github repo
-        'zip_url' => 'https://github.com/themeskult/lucid-theme/zipball/master', // the zip url of the github repo
-        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
-    );
-    new WP_GitHub_Updater($config);
-}
-
+include_once('inc/Theme-Updater/updater.php');
